70e575a8b601f844937e37f4a2a532c50548e07d
NAKAGAKI Kenji <nakaken0629@gmail.com>
2017-03-26 10:25:40 +0900

    modify to use Router

--- src/main/java/io/vertx/blog/first/MyFirstVerticle.java
+++ src/main/java/io/vertx/blog/first/MyFirstVerticle.java
@@ -1,5 +1,7 @@
 package io.vertx.blog.first;
 
+import io.vertx.ext.web.Router;
+import io.vertx.core.http.HttpServerResponse;
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Future;
 
@@ -7,23 +9,32 @@ public class MyFirstVerticle extends AbstractVerticle {
 
   @Override
   public void start(Future<Void> fut) {
-    vertx
-        .createHttpServer()
-        .requestHandler(r -> {
-          r.response().end("<h1>Hello from my first " +
-              "Vert.x 3 application</h1>");
-        })
-        .listen(
-            // Retrieve the port from the configuration,
-            // default to 8080.
-            config().getInteger("http.port", 8080),
-            result -> {
-              if (result.succeeded()) {
-                fut.complete();
-              } else {
-                fut.fail(result.cause());
-              }
-            }
-        );
+   // Create a router object.
+   Router router = Router.router(vertx);
+  
+   // Bind "/" to our hello message - so we are still compatible.
+   router.route("/").handler(routingContext -> {
+     HttpServerResponse response = routingContext.response();
+     response
+         .putHeader("content-type", "text/html")
+         .end("<h1>Hello from my first Vert.x 3 application</h1>");
+   });
+  
+   // Create the HTTP server and pass the "accept" method to the request handler.
+   vertx
+       .createHttpServer()
+       .requestHandler(router::accept)
+       .listen(
+           // Retrieve the port from the configuration,
+           // default to 8080.
+           config().getInteger("http.port", 8080),
+           result -> {
+             if (result.succeeded()) {
+               fut.complete();
+             } else {
+               fut.fail(result.cause());
+             }
+           }
+       );
   }
 }
